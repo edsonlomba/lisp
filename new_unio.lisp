a9e3612ce4d4a9377becedb340d6d1fc685da326
Edson Lomba <edsonlomba@gmail.com>
2017-11-17 07:06:59 -0200

    Comments
    Added comments

--- new_unio.lisp
+++ new_unio.lisp
@@ -1,17 +1,20 @@
-(defun find_member(x l)
+;Verifies if an element is a member of a list
+(defun is_member(x l)
 	(cond
 		((null l) nil)
 		((eql x (car l)))
-		(t(find_member x (cdr l)))))
+		(t(is_member x (cdr l)))))
 
+;removes an element of a list
 (defun remove_value(x l)
 	(cond
 		((null l) nil)
 		((eql x (car l))(remove_value x (cdr l)))
 		(t(cons (car l)(remove_value x (cdr l))))))
 
+;makes the union between 2 lists
 (defun new_union (l1 l2)
   (cond ((null l1) l2)
         ((null l2) l1)
-        ((find_member (car l1) l2)(cons (car l)(new-union (cdr l) (remove_value (car l1) l2))))
+        ((is_member (car l1) l2)(cons (car l)(new-union (cdr l) (remove_value (car l1) l2))))
         (t(cons (car l1) (new-union (cdr l1) l2)))))
